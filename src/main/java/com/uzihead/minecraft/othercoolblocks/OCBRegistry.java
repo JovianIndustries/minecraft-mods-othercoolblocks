93c275aff8fe1b19f5d604422c2e3d28669491f6
uzihead <sebastian.bularca@gmail.com>
2022-02-13 19:58:38 +0100

    finally added the first (non functional) but visible block to minecraft

--- src/main/java/com/uzihead/minecraft/othercoolblocks/OCBRegistry.java
+++ src/main/java/com/uzihead/minecraft/othercoolblocks/OCBRegistry.java
@@ -1,18 +1,22 @@
 package com.uzihead.minecraft.othercoolblocks;
 
 import com.uzihead.minecraft.othercoolblocks.block.ExpandedCraftingTable;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemNameBlockItem;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.IForgeRegistry;
+import net.minecraftforge.registries.IForgeRegistryEntry;
+import net.minecraftforge.registries.ObjectHolder;
 
-import java.util.Objects;
-
+import static com.uzihead.minecraft.othercoolblocks.OCBRegistry.Objects.crafting_station;
 import static com.uzihead.minecraft.othercoolblocks.OtherCoolBlocks.MODID;
 
 public class OCBRegistry{
@@ -20,21 +24,30 @@ public class OCBRegistry{
 // Event bus for receiving Registry Events)
     @Mod.EventBusSubscriber(bus=Mod.EventBusSubscriber.Bus.MOD)
     public static class RegistryEvents {
-        private static Block BLOCK_EXPANDED_CRAFTING_TABLE = null ;
+        private static Block block = null ;
 
         @SubscribeEvent
         public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
             // register a new block here
             OtherCoolBlocks.LOGGER.info(MODID + ":registerBlocks");
-            BLOCK_EXPANDED_CRAFTING_TABLE = new ExpandedCraftingTable(BlockBehaviour.Properties.of(Material.WOOD)).setRegistryName(MODID, "expanded_crafting_table");
-            blockRegistryEvent.getRegistry().register(BLOCK_EXPANDED_CRAFTING_TABLE);
-            OtherCoolBlocks.LOGGER.info("%sRegistered: ".formatted(MODID), BLOCK_EXPANDED_CRAFTING_TABLE.getName());
+            BlockBehaviour.Properties wood = BlockBehaviour.Properties.copy(Blocks.OAK_STAIRS).noOcclusion();
+            register(block = new ExpandedCraftingTable(wood), "expanded_crafting_table", blockRegistryEvent.getRegistry());
+            OtherCoolBlocks.LOGGER.info("%sRegistered: ".formatted(MODID), block.getName());
         }
 
         @SubscribeEvent
         public static void onItemRegistry(final RegistryEvent.Register<Item> event){
-            var blockItem = new ItemNameBlockItem(BLOCK_EXPANDED_CRAFTING_TABLE.setRegistryName(MODID, "expanded_crafting_table_tile"), new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS));
-            event.getRegistry().register(blockItem);
+            Item.Properties properties = new Item.Properties().tab(CreativeModeTab.TAB_DECORATIONS);
+            register(new BlockItem(block, properties), "expanded_crafting_table", event.getRegistry());
         }
+
+        private static <T extends IForgeRegistryEntry<T>> void register(T obj, String name, IForgeRegistry<T> registry) {
+            registry.register(obj.setRegistryName(new ResourceLocation(MODID, name)));
+        }
+    }
+
+    @ObjectHolder(MODID)
+    public static class Objects {
+        public static final Block crafting_station = null;
     }
 }
--- src/main/java/com/uzihead/minecraft/othercoolblocks/block/ExpandedCraftingTable.java
+++ src/main/java/com/uzihead/minecraft/othercoolblocks/block/ExpandedCraftingTable.java
@@ -1,10 +1,12 @@
 package com.uzihead.minecraft.othercoolblocks.block;
 
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.MaterialColor;
+import net.minecraftforge.common.extensions.IForgeBlockState;
 
 public final class ExpandedCraftingTable extends Block{
     public ExpandedCraftingTable(Properties prop) {
-        super(prop.color(MaterialColor.WOOD).destroyTime(1f).explosionResistance(1f));
+        super(prop.copy(Blocks.OAK_STAIRS).noOcclusion());
     }
 }
